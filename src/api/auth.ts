2eca1e072721ead1e8f724848b39eab47fff672e
sbchoi5451 <sbchoi5451@gmail.com>
2023-05-29 15:56:39 +0900

    fix. eslint fix

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -17,11 +17,11 @@ export interface SignupInfo {
 }
 
 export interface LoginInfo {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
 
-export const SignupApi = async (signupInfo: SignupInfo)=> {
+export const SignupApi = async (signupInfo: SignupInfo) => {
   try {
     await axiosInstance.post('/user/signup', signupInfo);
   } catch (error) {
@@ -32,10 +32,13 @@ export const SignupApi = async (signupInfo: SignupInfo)=> {
 
 export const LoginApi = async (loginInfo: LoginInfo) => {
   try {
-    const response: AxiosResponse<ApiResponse> = await axiosInstance.post('/user/login', loginInfo);
+    const response: AxiosResponse<ApiResponse> = await axiosInstance.post(
+      '/user/login',
+      loginInfo
+    );
     const accessToken = response.headers['access-token'];
     // Cookies.set("refreshToken", refreshToken)
   } catch (error) {
     if (axios.isAxiosError(error)) throw error;
   }
-}
+};
