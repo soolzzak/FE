d2ed88d23e3b5c2d29ecfbe580f69f5f725ef12f
elfinana <elfinana@naver.com>
2023-06-01 01:59:31 +0900

    Feat: 마이페이지추가

--- src/pages/StreamRoom.tsx
+++ src/pages/StreamRoom.tsx
@@ -41,62 +41,47 @@ export const StreamRoom = () => {
 
   const closeWelcome = () => {
     welcomeClose();
-  }
+  };
 
   return (
-    <div>
-      <div className="flex flex-col h-screen  rounded-3xl bg-[#cdcdcd]">
-        <div className="basis-1/12  flex justify-between p-5 m-5 mt-20">
-          <div className="flex flex-row items-center">
-            <div className="w-16 h-16 rounded-full bg-[#9A9A9A] mr-4" />
-            <p className="text-[20px] font-semibold mr-4">
-              카리나님과 따로 또 같이 혼술하는 중!
-            </p>
+    <div className="flex flex-col h-screen p-5 m-5 rounded-3xl bg-[#cdcdcd]">
+      <div className="basis-1/12 flex justify-between p-4">
+        <div className="flex flex-row items-center">
+          <div className="w-16 h-16 rounded-full bg-[#9A9A9A] mr-4" />
+          <p className="text-[20px] font-semibold mr-4">
+            카리나님과 따로 또 같이 혼술하는 중!
+          </p>
 
           <div className="flex flex-row gap-4 ">
             <Thumbdown />
             <Thumbup />
-            <Report setIsOpen={setIsOpen}/>
+            <Report setIsOpen={setIsOpen} />
           </div>
-          <p className="font-semibold text-[32px]">
-            얘기하면서 같이 소주마셔요!
-          </p>
         </div>
-        <div className="basis-11/12  grid grid-cols-5 grid-rows-6 gap-5">
-          <div className="col-span-3 row-span-6 bg-[#eae8e8]">상대방</div>
-          {/* <video
-            className="first-letter:col-span-2 row-span-3"
-            ref={myVideoRef}
-            autoPlay
-            muted
-          /> */}
-          <div className="col-span-2 row-span-3 bg-[#eae8e8]">나</div>
-          <div className="col-span-2 row-span-2 bg-[#eae8e8]">채팅</div>
-          <div className="col-span-2 row-span-1 bg-[#eae8e8]">텍스트보내기</div>
-        </div>
-        
         <p className="font-semibold text-[32px]">얘기하면서 같이 소주마셔요!</p>
       </div>
-      <div className="basis-11/12  grid grid-cols-5 grid-rows-6 gap-5">
+      <div className="basis-11/12 grid grid-cols-5 grid-rows-6 gap-5">
         <div className="col-span-3 row-span-6 bg-[#eae8e8]">상대방</div>
         <div className="col-span-2 row-span-3 bg-[#eae8e8]">나</div>
         <div className="col-span-2 row-span-2 bg-[#eae8e8]">채팅</div>
-        <div className="col-span-2 row-span-1 bg-[#eae8e8]" >텍스트보내기</div>
+        <div className="col-span-2 row-span-1 bg-[#eae8e8]">텍스트보내기</div>
       </div>
 
-      <Modal isOpen={isOpen} onClose={onClose} >
-        <ReportModal />
-      </Modal>
+      {/* <Modal isOpen={isOpen} onClose={onClose} >
+    <ReportModal />
+    </Modal> */}
 
       {/* <Modal isOpen={joinHostOpen} onClose={onJoinHostClose}>
-        <JoinHostModal onClose={onJoinHostClose}/>
-      </Modal> */}
+    <JoinHostModal onClose={onJoinHostClose}/>
+    </Modal> */}
 
-      <Modal isOpen={welcomeOpen} onClose={welcomeClose} >
-        <JoinPartnerModal onClose={closeWelcome}/>
-      </Modal>
-      <button type="button" onClick={() => setWelcomeOpen(true)}>임시버튼~_~</button>
+      {/* <Modal isOpen={welcomeOpen} onClose={welcomeClose} >
+    <JoinPartnerModal onClose={closeWelcome}/>
+    </Modal> */}
+
+      <button type="button" onClick={() => setWelcomeOpen(true)}>
+        임시버튼~_~
+      </button>
     </div>
-    
   );
 };
