aac7bcc6f8a8d42d23e62dcdc05faf1b03185f0c
sbchoi5451 <sbchoi5451@gmail.com>
2023-06-08 13:14:28 +0900

    Style: change primary color

--- src/components/Home/JoinRoomModal.tsx
+++ src/components/Home/JoinRoomModal.tsx
@@ -12,6 +12,7 @@ import {
   tabList,
 } from '../../utils/switchSelections';
 import { chatRoomInfoAtom } from './ChatroomCard';
+import { usernameAtom } from '../../store/mainpageStore';
 
 export const JoinRoomModal = () => {
   const myVideoRef = useRef<HTMLVideoElement>(null);
@@ -23,6 +24,7 @@ export const JoinRoomModal = () => {
   const [chatRoomInfo] = useAtom(chatRoomInfoAtom);
   const getCookie = Cookies.get('accessKey');
   const userInfo = jwtDecode<JwtPayload>(getCookie || '').auth;
+  console.log('d', userInfo);
 
   useEffect(() => {
     if (chatRoomInfo) {
@@ -52,13 +54,12 @@ export const JoinRoomModal = () => {
   }, []);
 
   return (
-    <div className="relative flex w-[1050px] h-[544.31px] bg-[#FFFFFF] rounded-[36px] justify-center items-center">
-      <div className="w-[60%] h-[90%] flex flex-col justify-between items-center">
-        <div className="text-2xl font-bold mt-5">
-          {chatRoomInfo?.title} 얘기하면서 같이 소주마셔요!
+    <div className="relative flex w-[1050px] h-[600px] bg-[#FFFFFF] rounded-[36px] justify-center items-center px-5">
+      <div className="w-[70%] h-[90%] flex flex-col justify-between items-center px-4">
+        <div className="w-[100%] h-[10%] text-left text-3xl font-bold truncate pl-1 pt-2">
+          {chatRoomInfo?.title}
         </div>
-
-        <div className="w-[90%] bg-[#D9D9D9] rounded-[20px]">
+        <div className="w-[100%] h-[90%] bg-[#D9D9D9] rounded-[20px]">
           <video
             className="w-full h-full object-cover rounded-[20px]"
             ref={myVideoRef}
@@ -68,55 +69,63 @@ export const JoinRoomModal = () => {
         </div>
       </div>
 
-      <div className="w-[40%] h-[90%] flex flex-col justify-between items-center">
-        <div className="flex items-center w-40 justify-center self-end gap-4 h-20 rounded-2xl bg-[#E0F5E6] mt-4 mr-12">
+      <div className="w-[40%] h-[90%] flex flex-col items-center self-center px-4">
+        <div className="w-[100%] h-[10%]" />
+        <div className="flex items-center w-40 justify-center self-end h-[15%] gap-4 rounded-2xl bg-[#E0F5E6]">
           <div>
             <img alt="userImg" className="w-14 h-14 rounded-full bg-black" />
           </div>
+
           <div>
             <p className="font-bold">곽준희님</p>
             <p className="font-bold">몇도</p>
           </div>
         </div>
-        <div className="flex flex-col self-start items-start">
-          {/* <p className="text-[#454545] font-semibold text-base mb-1">제목</p>
-          <div className=" font-bold flex justify-center items-center mb-5">
-            {chatRoomInfo?.title} 얘기하면서 같이 소주마셔요!
-          </div> */}
-          <p className="text-[#454545] font-semibold text-base mb-1">카테고리</p>
-          <div className="w-[120px] h-9 bg-primary-100 text-primary-300 rounded-md font-bold flex justify-center items-center mb-5">
-            {selections.map((tab, index) =>
-              tab === category ? tabList[index] : null
-            )}
+        <div className="w-[100%] h-[75%] flex flex-col justify-between ">
+          <div className='mt-20'>
+            <p className="text-[#454545] font-semibold text-base mb-1">
+              카테고리
+            </p>
+            <div className="w-[120px] h-9 bg-secondary-100 text-secondary-300 rounded-md font-bold flex justify-center items-center mb-5">
+              {selections.map((tab, index) =>
+                tab === category ? tabList[index] : null
+              )}
+            </div>
+            <p className="text-[#454545] font-semibold text-base mb-1">성별</p>
+            <div className="w-[120px] h-9 bg-secondary-100 text-secondary-300 rounded-md font-bold flex justify-center items-center mb-5">
+              {gender}
+            </div>
+
+            {chatRoomInfo?.isPrivate ? (
+              <>
+                <p className="text-[#454545] font-semibold text-base mb-1">
+                  비밀번호
+                </p>
+                <input
+                  type="password"
+                  className="border border-secondary-300 rounded-md h-9"
+                />
+              </>
+            ): null}
           </div>
-          <p className="text-[#454545] font-semibold text-base mb-1">성별</p>
-          <div className="w-[120px] h-9 bg-primary-100 text-primary-300 rounded-md font-bold flex justify-center items-center mb-5">
-            {gender}
-          </div>
-          {/* <p className="text-[#454545] font-semibold text-base mb-1">비밀번호</p> */}
-          {/* <div className="flex items-center justify-center rounded-xl w-64 h-12 bg-[#D9D9D9] text-[18px] mb-3">
+          <button
+            type="button"
+            className="w-[100%] h-16 bg-primary-300 rounded-2xl text-[#FFFFFF] text-[22px] font-bold hover:bg-primary-400"
+            onClick={() => {
+              setIsOpenJoinRoom(false);
+              setIsOpenWaitingRoom(true);
+            }}
+          >
+            혼술짝 방 입장하기
+          </button>
+        </div>
+        {/* <div className="flex items-center justify-center rounded-xl w-64 h-12 bg-[#D9D9D9] text-[18px] mb-3">
             누르면 5초 후에 입장해요😊
           </div> */}
-          {/* {chatRoomInfo?.isPrivate ? null : (
-            <input
-            type="password"
-            className="border" />
-          )} */}
-        </div>
-        <button
-          type="button"
-          className="w-96 h-16 bg-primary-300 rounded-2xl text-[#FFFFFF] text-[22px] font-bold hover:bg-primary-400"
-          onClick={() => {
-            setIsOpenJoinRoom(false);
-            setIsOpenWaitingRoom(true);
-          }}
-        >
-          혼술짝 방 입장하기
-        </button>
       </div>
       <div
         role="none"
-        className="absolute right-3 top-3 hover:cursor-pointer"
+        className="absolute right-3 top-2 hover:cursor-pointer"
         onClick={() => setIsOpenJoinRoom(false)}
       >
         <DeleteBtn />
