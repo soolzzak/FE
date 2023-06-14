347c69ea49ffc9245512bf8efc05aef55a065086
sbchoi5451 <sbchoi5451@gmail.com>
2023-06-14 23:43:35 +0900

    Fix: streamroom 코드 수정

--- src/components/StreamRoom/KickoutModal.tsx
+++ src/components/StreamRoom/KickoutModal.tsx
@@ -1,25 +1,28 @@
 import { CancelButton } from '../common/CancelButton';
 
-export const KickoutModal = ({ onClose }: { onClose: () => void }) => (
-  <div className="grid grid-cols-1 justify-center items-center">
-    <div className="w-[409px] h-[202px] relative  rounded-2xl bg-white flex flex-col justify-center items-center">
-      <p className="font-semibold text-[24px]">카리나님을 강퇴하시겠어요?</p>
-      <div className="flex justify-center items-center gap-4 mt-10">
-        <button
-          onClick={onClose}
-          type="button"
-          className="border rounded-lg w-[139px] h-[44px] text-[16px] text-[#827676] "
-        >
-          아니요
-        </button>
-        <button
-          type="button"
-          className="border rounded-lg w-[139px] h-[44px] text-[16px] text-[#827676] "
-        >
-          네, 강퇴할게요
-        </button>
+export const KickoutModal = ({ onClose }: { onClose: () => void }) => {
+  const a = 'b';
+  return (
+    <div className="grid grid-cols-1 justify-center items-center">
+      <div className="w-[409px] h-[202px] relative  rounded-2xl bg-white flex flex-col justify-center items-center">
+        <p className="font-semibold text-[24px]">카리나님을 강퇴하시겠어요?</p>
+        <div className="flex justify-center items-center gap-4 mt-10">
+          <button
+            onClick={onClose}
+            type="button"
+            className="border rounded-lg w-[139px] h-[44px] text-[16px] text-[#827676] "
+          >
+            아니요
+          </button>
+          <button
+            type="button"
+            className="border rounded-lg w-[139px] h-[44px] text-[16px] text-[#827676] "
+          >
+            네, 강퇴할게요
+          </button>
+        </div>
       </div>
+      <CancelButton onClose={onClose} />
     </div>
-    <CancelButton onClose={onClose} />
-  </div>
-);
+  );
+};
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { NotFoundIcon } from '../assets/svgs/NotFound';
 
 export const NotFound = () => (
-  <div className="f-jic h-screen">
-    <div>404 Page Not Found!</div>
+  <div className="f-jic-col h-screen gap-4">
+    <div className="f-jic text-8xl font-extrabold text-secondary-300 gap-2">
+      404
+      <NotFoundIcon />
+    </div>
+
+    <div className='text-2xl font-bold'>존재하지 않는 페이지 입니다.</div>
+
+    <button type="button" className='w-56 h-11 bg-primary-300 text-white text-xl rounded-lg'>홈 화면으로 돌아가기</button>
   </div>
 );
