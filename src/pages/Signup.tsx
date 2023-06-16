afdd8e8208848c1fe58c3e27366434fd19724859
JungHBae <fruitsforthought@gmail.com>
2023-06-17 07:26:34 +0900

    fix: signup background color

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,9 +1,9 @@
 import { SignupInput } from '../components/signup/SignupInput';
 
 export const Signup = () => (
-  <div className="flex flex-col items-center gradient">
-    <div className="bg-white mt-24 mb-4 p-12 rounded-xl">
-      <div className="text-2xl font-bold">회원가입</div>
+  <div className="flex flex-col items-center grad h-screen">
+    <div className="bg-white mt-24 mb-4 px-12 py-8 rounded-xl">
+      <div className="text-2xl font-bold mb-3">회원가입</div>
       <SignupInput />
     </div>
   </div>
