e4caa3ab534102c0aebdf344080155897b85737a
JungHBae <fruitsforthought@gmail.com>
2023-06-22 23:26:54 +0900

    feat: streamroom snapshot

--- src/components/Home/WaitingRoomModal.tsx
+++ src/components/Home/WaitingRoomModal.tsx
@@ -5,21 +5,32 @@ import { CancelButton } from '../common/CancelButton';
 import { isOpenWaitingAtom } from '../../store/modalStore';
 import { chatRoomInfoAtom } from './ChatroomCard';
 
-export const WaitingRoomModal = () => {
-  const [timeLeft, setTimeLeft] = useState(3);
+export const WaitingRoomModal = ({
+  count,
+  onClose,
+  startAfterTimeout,
+}: {
+  count?: number;
+  onClose?: () => void;
+  startAfterTimeout?: () => void;
+}) => {
+  const [timeLeft, setTimeLeft] = useState(5);
   const navigation = useNavigate();
-  const [, setIsOpenWaitingRoom] = useAtom(isOpenWaitingAtom)
-  const [chatRoomInfo] =useAtom(chatRoomInfoAtom)
-
+  const [, setIsOpenWaitingRoom] = useAtom(isOpenWaitingAtom);
+  const [chatRoomInfo] = useAtom(chatRoomInfoAtom);
+  console.log(count);
   useEffect(() => {
     const timer = setInterval(() => {
       setTimeLeft((time) => time - 1);
     }, 1000);
 
-    if (timeLeft === 0) {
+    if (timeLeft === 0 && !count) {
       clearInterval(timer);
-      setIsOpenWaitingRoom(false)
-      navigation(`/room/${chatRoomInfo?.roomId}`)
+      setIsOpenWaitingRoom(false);
+      navigation(`/room/${chatRoomInfo?.roomId}`);
+    } else if (timeLeft === 0 && onClose && startAfterTimeout) {
+      startAfterTimeout();
+      onClose();
     }
 
     return () => {
@@ -27,12 +38,20 @@ export const WaitingRoomModal = () => {
     };
   }, [timeLeft, setIsOpenWaitingRoom]);
 
-
   return (
-    <div className="relative w-[600px] h-[350px] bg-[#FFFFFF] rounded-[30px] border text-center flex flex-col justify-center items-center">
-      <div className="font-bold text-2xl text-[#454545] mb-2">
-        {timeLeft}초 뒤에 {chatRoomInfo?.username}님의 방으로 입장합니다!
-      </div>
+    <div
+      className={`relative bg-[#FFFFFF] rounded-[30px] border text-center flex flex-col justify-center items-center
+      ${count ? 'w-[450px] h-[250px]' : 'w-[600px] h-[350px]'}`}
+    >
+      {count ? (
+        <div className="font-bold text-2xl text-[#454545] mb-2">
+          {timeLeft}초 뒤에 사진이 찍혀요!
+        </div>
+      ) : (
+        <div className="font-bold text-2xl text-[#454545] mb-2">
+          {timeLeft}초 뒤에 {chatRoomInfo?.username}님의 방으로 입장합니다!
+        </div>
+      )}
 
       <div className="flex justify-center items-center w-20 h-20 font-bold text-[40px] text-[#454545] mb-2 border border-[#AEAEAE] rounded-full">
         {timeLeft}
@@ -42,14 +61,16 @@ export const WaitingRoomModal = () => {
         매너와 예의를 지키며 즐겁게 혼술짝 해요😊
       </div>
 
-      <div className="w-[472.23px] h-[90px] bg-[#FAFAFA] flex flex-col justify-center">
-        <p className="font-medium text-lg text-[#454545]">
-          바르고 고운말은 기본!
-        </p>
-        <p className="font-medium text-lg text-[#454545]">
-          예의없는 행동시 신고 대상이 됩니다.
-        </p>
-      </div>
+      {!count && (
+        <div className="w-[472.23px] h-[90px] bg-[#FAFAFA] flex flex-col justify-center">
+          <p className="font-medium text-lg text-[#454545]">
+            바르고 고운말은 기본!
+          </p>
+          <p className="font-medium text-lg text-[#454545]">
+            예의없는 행동시 신고 대상이 됩니다.
+          </p>
+        </div>
+      )}
       <CancelButton onClose={() => setIsOpenWaitingRoom(false)} />
     </div>
   );
--- src/components/StreamRoom/Modals/PhotoConfirmModal.tsx
+++ src/components/StreamRoom/Modals/PhotoConfirmModal.tsx
@@ -3,21 +3,34 @@ import { CommonButton } from '../../common/CommonButton';
 
 export const PhotoConfirmModal = ({
   sendPhotoConfirmMessage,
+  sendPhotoDenyMessage,
   setOpenConfirmModalIsOpen,
 }: {
   sendPhotoConfirmMessage: () => void;
+  sendPhotoDenyMessage: () => void;
   setOpenConfirmModalIsOpen: Dispatch<SetStateAction<boolean>>;
 }) => (
-  <div className="f-jic-col px-5 py-3 rounded-2xl bg-white">
+  <div className="f-jic-col px-7 py-5 rounded-2xl bg-white">
     <span>사진찍기에 동의하시겠습니까?</span>
-    <CommonButton
-      enabled
-      clickHandler={() => {
-        sendPhotoConfirmMessage();
-        setOpenConfirmModalIsOpen(false);
-      }}
-      buttonText="동의하기"
-      dimensions="mt-3"
-    />
+    <div className="flex gap-3">
+      <CommonButton
+        enabled
+        clickHandler={() => {
+          sendPhotoConfirmMessage();
+          setOpenConfirmModalIsOpen(false);
+        }}
+        buttonText="동의하기"
+        dimensions="mt-3"
+      />
+      <CommonButton
+        enabled
+        clickHandler={() => {
+          sendPhotoDenyMessage();
+          setOpenConfirmModalIsOpen(false);
+        }}
+        buttonText="동의하지 않기"
+        dimensions="mt-3"
+      />
+    </div>
   </div>
 );
