dfa81549e424e961f06b0d3bead80f048c5f7f1f
JungHBae <fruitsforthought@gmail.com>
2023-06-30 15:31:45 +0900

    fix: header z-index 30

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,7 +2,7 @@ import { HeaderRightSection } from '../components/Header/HeaderRightSection';
 import { HeaderLeftSection } from '../components/Header/HeaderLeftSection';
 
 export const Header = () => (
-  <div className="fixed sm:f-jic w-full bg-white z-50 shadow-sm min-w-[520px]">
+  <div className="fixed sm:f-jic w-full bg-white z-30 shadow-sm min-w-[520px]">
     <div className="relative flex sm:f-ic max-w-[1600px]  w-full xl:px-24 justify-between h-[70px]">
       <HeaderLeftSection />
       <HeaderRightSection />
